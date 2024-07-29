a8e25c390b4115460a308060988dfed8d34d0f87
GökhanAYDIN <gokhanaydin_243944@hotmail.com>
2024-07-29 21:39:08 +0300

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+var dogruIcon = const Icon(
+  Icons.mood,
+  color: Colors.green,
+);
+
+var yanlisIcon = const Icon(
+  Icons.mood_bad,
+  color: Colors.red,
+);
